321f3a96c4088512b9f21c8228d6ec7f67b2f5ff
jekson tambunan <108537531+jekson11@users.noreply.github.com>
2023-08-12 21:25:48 +0700

    Update MapperFeatureTest.java
    delete comment

--- test/java/tutorial/javajson/MapperFeatureTest.java
+++ test/java/tutorial/javajson/MapperFeatureTest.java
@@ -10,14 +10,8 @@ public class MapperFeatureTest {
     @Test
     void testMapperFeature() throws JsonProcessingException {
         ObjectMapper mapper = new ObjectMapper()
-                //dengan menambahkan yang di bawah ini saat pembuatan ObjectMapper
                 .configure(MapperFeature.ACCEPT_CASE_INSENSITIVE_PROPERTIES, true);
-        //makan akan di biarkan kita membuat object person dengan field yang huruf besar dan kecilnya
-        //tidak sama dengan field yang ada di class person kalau defaultnya ini false jadi tidak di biarkan/tidak bisa
-        //coba aja hapus kalau tidak percaya dan masih banyak lagi MapperFeature nya
-
-        //ini json nya boleh dari file juga y
-        String json = """
+       String json = """
                 {
                 "FirstName":"jekson",
                 "LastName":"tambunan"
